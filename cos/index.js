462a9c4e35d05c2e4e10286b0abcd6cb6f7c1a8a
erikoui <eriksofs1@hotmail.com>
2020-10-16 17:22:37 +0300

    MD5 filenames when uploading to cloud

--- cos/index.js
+++ cos/index.js
@@ -45,93 +45,94 @@ class Cloud {
                 Key: itemName
             }).promise();
             if (data != null) {
-                return cb(null,data);
+                return cb(null, data);
             }
         } catch (e) {
             //TODO: put this error in the callback
             console.error(`ERROR: ${e.code} - ${e.message}\n`);
-            return cb(e,null)
+            return cb(e, null)
         }
+    }
+
+    async simpleUpload(bucketName, itemName, filePath) {
+        console.log("Starting upload of "+itemName)
+        await this.cos.upload({
+            Bucket: bucketName,
+            Key: itemName,
+            Body: fs.createReadStream(filePath),
+        }).promise()
+            .then((data) => {
+                console.log("file " + itemName + " uplaoded.")
+            })
+            .catch((err) => {
+                console.error(`ERROR: ${e.code} - ${e.message}\n`);
+            });
     }
 
     async multiPartUpload(bucketName, itemName, filePath) {
         var uploadID = null;
-    
+
         if (!fs.existsSync(filePath)) {
             log.error(new Error(`The file \'${filePath}\' does not exist or is not accessible.`));
             return;
         }
-    
+
         console.log(`Starting multi-part upload for ${itemName} to bucket: ${bucketName}`);
         return this.cos.createMultipartUpload({
             Bucket: bucketName,
             Key: itemName
         }).promise()
-        .then((data) => {
-            uploadID = data.UploadId;
-    
-            //begin the file upload        
-            fs.readFile(filePath, (e, fileData) => {
-                //min 5MB part
-                var partSize = 1024 * 1024 * 5;
-                var partCount = Math.ceil(fileData.length / partSize);
-    
-                async.timesSeries(partCount, (partNum, next) => {
-                    var start = partNum * partSize;
-                    var end = Math.min(start + partSize, fileData.length);
-    
-                    partNum++;
-    
-                    console.log(`Uploading to ${itemName} (part ${partNum} of ${partCount})`);  
-    
-                    this.cos.uploadPart({
-                        Body: fileData.slice(start, end),
-                        Bucket: bucketName,
-                        Key: itemName,
-                        PartNumber: partNum,
-                        UploadId: uploadID
-                    }).promise()
-                    .then((data) => {
-                        next(e, {ETag: data.ETag, PartNumber: partNum});
-                    })
-                    .catch((e) => {
-                        cancelMultiPartUpload(bucketName, itemName, uploadID);
-                        console.error(`ERROR: ${e.code} - ${e.message}\n`);
-                    });
-                }, (e, dataPacks) => {
-                    this.cos.completeMultipartUpload({
-                        Bucket: bucketName,
-                        Key: itemName,
-                        MultipartUpload: {
-                            Parts: dataPacks
-                        },
-                        UploadId: uploadID
-                    }).promise()
-                    .then(console.log(`Upload of all ${partCount} parts of ${itemName} successful.`))
-                    .catch((e) => {
-                        cancelMultiPartUpload(bucketName, itemName, uploadID);
-                        console.error(`ERROR: ${e.code} - ${e.message}\n`);
+            .then((data) => {
+                uploadID = data.UploadId;
+
+                //begin the file upload        
+                fs.readFile(filePath, (e, fileData) => {
+                    //min 5MB part
+                    var partSize = 1024 * 1024 * 5;
+                    var partCount = Math.ceil(fileData.length / partSize);
+
+                    async.timesSeries(partCount, (partNum, next) => {
+                        var start = partNum * partSize;
+                        var end = Math.min(start + partSize, fileData.length);
+
+                        partNum++;
+
+                        console.log(`Uploading to ${itemName} (part ${partNum} of ${partCount})`);
+
+                        this.cos.uploadPart({
+                            Body: fileData.slice(start, end),
+                            Bucket: bucketName,
+                            Key: itemName,
+                            PartNumber: partNum,
+                            UploadId: uploadID
+                        }).promise()
+                            .then((data) => {
+                                next(e, { ETag: data.ETag, PartNumber: partNum });
+                            })
+                            .catch((e) => {
+                                cancelMultiPartUpload(bucketName, itemName, uploadID);
+                                console.error(`ERROR: ${e.code} - ${e.message}\n`);
+                            });
+                    }, (e, dataPacks) => {
+                        this.cos.completeMultipartUpload({
+                            Bucket: bucketName,
+                            Key: itemName,
+                            MultipartUpload: {
+                                Parts: dataPacks
+                            },
+                            UploadId: uploadID
+                        }).promise()
+                            .then(console.log(`Upload of all ${partCount} parts of ${itemName} successful.`))
+                            .catch((e) => {
+                                cancelMultiPartUpload(bucketName, itemName, uploadID);
+                                console.error(`ERROR: ${e.code} - ${e.message}\n`);
+                            });
                     });
                 });
+            })
+            .catch((e) => {
+                console.error(`ERROR: ${e.code} - ${e.message}\n`);
             });
-        })
-        .catch((e) => {
-            console.error(`ERROR: ${e.code} - ${e.message}\n`);
-        });
-    }
-    
-    async cancelMultiPartUpload(bucketName, itemName, uploadID) {
-        return this.cos.abortMultipartUpload({
-            Bucket: bucketName,
-            Key: itemName,
-            UploadId: uploadID
-        }).promise()
-        .then(() => {
-            console.log(`Multi-part upload aborted for ${itemName}`);
-        })
-        .catch((e)=>{
-            console.error(`ERROR: ${e.code} - ${e.message}\n`);
-        });
     }
 }
 module.exports = Cloud
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const { db } = require('./db');
 var fs = require("fs")
-
+const md5File = require('md5-file')
 var childProcess = require('child_process');
 const passport = require('passport'), Strategy = require('passport-local').Strategy;
 const errorHandler = require('./_helpers/error-handler');
@@ -22,20 +22,20 @@ const PORT = process.env.PORT || 5000
 // that the password is correct and then invoke `cb` with a user object, which
 // will be set at `req.user` in route handlers after authentication.
 passport.use(new Strategy(
-  async function (username, password, cb) {
+  async (username, password, cb) => {
     //db.users.testDb();
     let user;
     try {
       user = await db.users.findByName(username);
       if (!user) {
-        console.log("Invalid login")
+        console.log("Invalid login");
         return cb(null, false, { message: 'No user by that name' });
       }
     } catch (e) {
       return cb(e);
     }
-    console.log("Found user " + user.uname)
-    return cb(null, user)
+    console.log("Found user " + user.uname);
+    return cb(null, user);
   }
 ));
 
@@ -69,19 +69,20 @@ function runScript(scriptPath, args, messagecb, warningcb, donecb) {
   var output = ''
 
   // listen for errors as they may prevent the exit event from firing
-  process.on('warning', function (err) {
+  process.on('warning', (err) => {
     warningcb(err);
   });
 
   //listen for general messages (from process.send)
-  process.on('message', function (data) {
-    if(data)
-      messagecb(data)
+  process.on('message', (data) => {
+    if (data)
+      messagecb(data);
   });
 
   // execute the messagecb once the process has finished running
-  process.on('exit', function (code) {
-    if (invoked) return;
+  process.on('exit', (code) => {
+    if (invoked)
+      return;
     invoked = true;
     var err = code === 0 ? null : new Error('exit code ' + code);
     messagecb(output);
@@ -105,15 +106,15 @@ express()
   .get('/download', (req, res) => {
     //thread is set by the url (e.g .../download?thread=https://boards.4chan.org/sp/thread/103)
     let thread = req.query.thread
-    let output = { log: [] ,filenames:[]}
+    let output = { log: [], filenames: [] }
     runScript(chanDownloader, [thread],
       (msg) => {
         //This runs each time the script calls process.send
 
         //Save the massage to the approptiate JSON tag
-        if(msg.log)//log message
+        if (msg.log)//log message
           output.log.push(msg.log);
-        if(msg.filenames)//chanDownloader sent a filename 
+        if (msg.filenames)//chanDownloader sent a filename 
           output.filenames.push(msg.filenames);
       },
       (warn) => {
@@ -125,15 +126,26 @@ express()
         console.log(output)
         for (var i = 0; i < output.log.length; i++) {
           if (output.log[i])//ignore undefined so that the server doesnt crash
-            res.write(output.log[i]+"\n")
+            res.write(output.log[i] + "\n")
         }
 
-        for(var i=0;i<output.filenames.length;i++){
-          //TODO: get md5 of file
-          md5=path.parse(output.filenames[i]).name;
-          ext=path.parse(output.filenames[i]).ext;
-          cloud.multiPartUpload('s-tashe-ibm-storage',md5+ext,output.filenames[i]);
+        for (var i = 0; i < output.filenames.length; i++) {
+          //calculates MD5 of each pic asynchronously and uploads it when done, to prevent duplicate file uploads
+          //DO NOT CHANGE CONST. IT ENSURES FILEPATH IS PASSED TO THE .then()
+          const filePath=path.join(output.filenames[i])
+          md5File(filePath).then((md5) => {
+            ext = path.parse(filePath).ext;
+            cloud.simpleUpload('s-tashe-ibm-storage', md5 + ext, filePath);
+          }).catch((reason) => {
+            console.log("error with md5 while uploading to cloud:" + reason)
+          })
         }
+        
+        //upload is done in a separate function to bind the filePath else it will just upload the last image many times instead of all images one time
+        function upload(filePath,md5){
+          
+        }
+
         //TODO: res.render(pages/chandownloaderoutput,{status: err}) or something
         res.end()
       })
