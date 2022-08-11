56a7622fe714fc958a08067f1e36ffa9c107a962
erikoui <eriksofs@hotmail.com>
2022-08-11 20:54:30 +0300

    stuff

--- _helpers/db2/config.js
+++ _helpers/db2/config.js
@@ -2,12 +2,12 @@ const env = process.env;
 
 const config = {
   db: {/* do not put password or any sensitive info here, done only for demo */
-    password: env.DB_PASSWORD||'FuckMyLife69',
-    user: env.DB_USER || 'u658055400_erikprojects',
-    database: env.DB_NAME || 'u658055400_erikprojects',
-    host: env.DB_HOST ||'2.57.89.103',
+    password: env.DB_PASSWORD,
+    user: env.DB_USER,
+    database: env.DB_NAME,
+    host: env.DB_HOST,
     waitForConnections: true,
-    connectionLimit: env.DB_CONN_LIMIT || 2,
+    connectionLimit: env.DB_CONN_LIMIT,
     queueLimit: 0,
     debug: env.DB_DEBUG || false,
   },
